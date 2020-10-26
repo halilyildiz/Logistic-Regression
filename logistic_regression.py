ab73fbd88aee2e5742ceccda89f677e092a8dab8
halilyildiz <halilyildiz.dev@gmail.com>
2020-10-26 09:06:28 +0300

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -26,7 +26,7 @@ x_data = data.drop(["diagnosis"],axis = 1)
 x = (x_data-np.min(x_data))/(np.max(x_data)-np.min(x_data))
 # (x - min(x))/(max(x)-min(x))
 
-#%% split data
+#%% split data---
 from sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2, random_state = 42)
